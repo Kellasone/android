13c51ec6df5688f02b3c244fed8cf6c573451df0
Alexandru Mihai <a_mihai_lo@yahoo.ro>
2020-04-28 19:30:57 +0300

    Main fragment and main activity. login fragment and activity

--- app/src/main/java/com/example/androidfinal/activities/MainActivity.java
+++ app/src/main/java/com/example/androidfinal/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.androidfinal;
+package com.example.androidfinal.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.androidfinal.R;
 
 public class MainActivity extends AppCompatActivity {
 
